698a28c2fab7dbfd8626261449dc1bd68405169f
Radoslav Georgiev <radoslavlgeorgiev@gmail.com>
2017-11-02 13:48:43 +0200

    new scaffolded job + Auth. + UTs

---
+++ MonsterValueCrew/MonsterValueCrew.Tests/Controllers/HomeControllerTests/Index_Should_Dummy.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonsterValueCrew.Tests.Controllers.HomeControllerTests
+{
+    [TestClass]
+    public  class Index_Should_Dummy
+    {
+        [TestMethod]
+        public void ShouldBeSuccessfull()
+        {
+            bool success = true;
+            Assert.IsTrue(success);
+        }
+    }
+}
