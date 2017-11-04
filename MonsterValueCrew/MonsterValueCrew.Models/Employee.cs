347cfa4e35f7cce2ce7c73a45bf3e3afa111ccda
Georgi Angelov <georgi.n.angelov@gmail.com>
2017-11-04 17:17:27 +0200

    Poco models init commit

---
+++ MonsterValueCrew/MonsterValueCrew.Models/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MonsterValueCrew.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(50,
+            ErrorMessage = "Enter a username between 3 and 50 symbols",
+            MinimumLength = 3)]
+        public string LastName { get; set; }
+
+        public int DepartmentId { get; set; }
+
+        public virtual Department Department { get; set; }
+
+        public int RoleId { get; set; }
+
+        public virtual Role Role { get; set; }
+
+
+
+    }
+}
---
+++ MonsterValueCrew/MonsterValueCrew.Models/UserCourseAssignment.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MonsterValueCrew.Models
+{
+    public class UserCourseAssignment
+    {
+        [Required]
+        public DateTime DueDate { get; set; }
+
+        [Required]
+        public bool IsMandatory { get; set; }
+
+        public bool IsAssigned { get; set; }
+
+        public int EmployeeId { get; set; }
+
+        public virtual Employee Employee { get; set; }
+
+        public int CourseId { get; set; }
+
+        public virtual Course Course { get; set; }
+        
+        public StatusName Name { get; set; }
+
+    }
+}
