fa4cbe239948ca76e9c79b2cbf0e157d6f7197af
radoslavlg <radoslavlgeorgiev@gmail.com>
2017-11-06 10:17:51 +0200

    Identity transferred to .Data folder;

--- MonsterValueCrew/MonsterValueCrew/App_Start/IdentityConfig.cs
+++ MonsterValueCrew/MonsterValueCrew/App_Start/IdentityConfig.cs
@@ -11,6 +11,8 @@ using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using Microsoft.Owin.Security;
 using MonsterValueCrew.Models;
+using MonsterValueCrew.Data.Models;
+using MonsterValueCrew.Data;
 
 namespace MonsterValueCrew
 {
--- MonsterValueCrew/MonsterValueCrew/Controllers/AccountController.cs
+++ MonsterValueCrew/MonsterValueCrew/Controllers/AccountController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using MonsterValueCrew.Models;
+using MonsterValueCrew.Data.Models;
 
 namespace MonsterValueCrew.Controllers
 {
