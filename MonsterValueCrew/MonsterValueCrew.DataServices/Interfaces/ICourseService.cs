eac7dd6838d5e24d0f5e5cfad3f16b9a3f9541b3
Radoslav Georgiev <radoslavlgeorgiev@gmail.com>
2017-11-09 15:31:57 +0200

    DataServices Project; ICourseService; Assigmentdatamodel

---
+++ MonsterValueCrew/MonsterValueCrew.DataServices/Interfaces/ICourseService.cs
@@ -0,0 +1,33 @@
+﻿using MonsterValueCrew.Data.DataModels;
+using MonsterValueCrew.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonsterValueCrew.DataServices.Interfaces
+{
+    public interface ICourseService
+    {
+        IEnumerable<Course> GetAllCourses();
+
+        IEnumerable<Course> GetCoursesByUserName(string username);
+
+        Course GetCourseById(int courseId);
+
+        Task AddCourseObjectToDb(Course course);
+
+        //Do we need to add CourseId here or not?
+        Task AddCourseToDb(string description, DateTime DateAdded, int passScore, bool isDeleted);
+
+        Task AssignCourseToUser(string userName, int courseId,
+            bool isAssigned, bool isMandatory, DateTime dueDate);
+
+        Task AssignCourseToDepartment(int departmentID, int courseId,
+            bool isAssigned, bool isMandatory, DateTime dueDate);
+
+        //We need a model here
+        IEnumerable<UserCourseAssignmentModel> GetInfoCoursesForUser(string username);
+    }
+}
