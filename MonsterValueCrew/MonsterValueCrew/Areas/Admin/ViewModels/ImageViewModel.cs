1a843e0ae41fb07ee1c861079ec289534f635f6c
Georgi Angelov <georgi.n.angelov@gmail.com>
2017-11-09 16:03:35 +0200

    Added new AdminViewModels

---
+++ MonsterValueCrew/MonsterValueCrew/Areas/Admin/ViewModels/ImageViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace MonsterValueCrew.Areas.Admin.ViewModels
+{
+    public class ImageViewModel
+    {
+        public string Name { get; set; }
+
+        public byte[] ImageInBase64 { get; set; }
+
+        public int Order { get; set; }
+    }
+}
---
+++ MonsterValueCrew/MonsterValueCrew/Areas/Admin/ViewModels/UserViewModel.cs
@@ -0,0 +1,36 @@
+﻿using MonsterValueCrew.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+
+namespace MonsterValueCrew.Areas.Admin.Models
+{
+    public class UserViewModel
+    {
+        public string Id { get; set; }
+
+        public string UserName { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public bool IsAdmin { get; set; }
+
+        public static Expression<Func<ApplicationUser, UserViewModel>> Create
+        {
+            get
+            {
+                return u => new UserViewModel()
+                {
+                    Id = u.Id,
+                    UserName = u.UserName,
+                    FirstName = u.FirstName,
+                    LastName = u.LastName
+                };
+            }
+        }
+    }
+}
