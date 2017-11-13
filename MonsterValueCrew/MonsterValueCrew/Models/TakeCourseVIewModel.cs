d84452084dc9084c026948170e758b322b7cb7ae
Radoslav Georgiev <radoslavlgeorgiev@gmail.com>
2017-11-13 14:42:22 +0200

    Some more changes in layout and courses: controller, views and other

---
+++ MonsterValueCrew/MonsterValueCrew/Models/TakeCourseVIewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MonsterValueCrew.Data.Models;
+
+namespace MonsterValueCrew.Models
+{
+    public class TakeCourseVIewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public virtual ICollection<Image> Images { get; set; }
+
+    }
+}
