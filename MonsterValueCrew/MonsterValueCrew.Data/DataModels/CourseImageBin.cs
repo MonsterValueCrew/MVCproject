44b1df6a59e28cf7fd65959ebb8c1ad001cd3a56
radoslavlg <radoslavlgeorgiev@gmail.com>
2017-11-15 10:41:50 +0200

    Changes in CourseCrudService - GetAllSlidesForCourse added; private Run method removed

---
+++ MonsterValueCrew/MonsterValueCrew.Data/DataModels/CourseImageBin.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonsterValueCrew.Data.DataModels
+{
+    public class CourseImageBin
+    {
+        public byte[] ImageBinary { get; set; }
+    }
+}
--- MonsterValueCrew/MonsterValueCrew.DataServices/Course/Contracts/ICourseCrudService.cs
+++ MonsterValueCrew/MonsterValueCrew.DataServices/Course/Contracts/ICourseCrudService.cs
@@ -1,4 +1,5 @@
-﻿using MonsterValueCrew.Data.Models;
+﻿using MonsterValueCrew.Data.DataModels;
+using MonsterValueCrew.Data.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,6 +17,8 @@ namespace MonsterValueCrew.DataServices.Interfaces
         Course GetCourseById(int courseId);
 
         string GetCourseName(int courseId);
+
+        ICollection<CourseImageBin> GetAllSlidesForCourse(int courseId);
 
         IList<Image> GetImages(int courseId);
 
